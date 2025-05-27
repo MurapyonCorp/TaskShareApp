ff45abd97ce725fe2eb2d59aef4380c573b66269
MurapyonCorp <82795887+MurapyonCorp@users.noreply.github.com>
2025-05-27 21:45:34 +0900

    サインアップのバリデーション作成

--- backend/src/auth/dto/auth.dto.ts
+++ backend/src/auth/dto/auth.dto.ts
@@ -1,1 +1,22 @@
-export class AuthDto {}
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+import { Match } from './match.decorator';
+
+export class AuthDto {
+  @IsString()
+  @IsNotEmpty({ message: '入力必須です' })
+  name: string;
+
+  @IsEmail({}, { message: 'メールアドレスが無効です' })
+  @IsNotEmpty({ message: '入力必須です' })
+  email: string;
+
+  @IsString()
+  @IsNotEmpty({ message: '入力必須です' })
+  @MinLength(6)
+  password: string;
+
+  @IsString()
+  @IsNotEmpty({ message: '入力必須です' })
+  @Match('password', { message: 'パスワードが一致していません' })
+  confirmPassword: string;
+}
