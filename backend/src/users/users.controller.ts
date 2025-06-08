b1f6cdd45e7c31224e180b559412103336deaf8c
MurapyonCorp <82795887+MurapyonCorp@users.noreply.github.com>
2025-06-08 14:33:03 +0900

    users作成

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,29 +1,23 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
 
 @Controller('users')
 export class UsersController {
+  // UsersService をDI（依存性注入）している。これによりコントローラー内でサービスのメソッドが使えるようになる。
   constructor(private readonly usersService: UsersService) {}
 
   @Get()
-  findAll() {
-    return this.usersService.findAll();
+  async findAll(): Promise<User[]> {
+    // this.usersService.findAll() を呼び出し、DB内の全ユーザー情報を取得して返す。
+    // 戻り値は User[] 型のPromise（非同期でユーザー配列を返す）。
+    return await this.usersService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
+  @Get(':id/profile')
+  async getProfile(@Param('id') id: string) {
+    // +id により数値に変換してから getProfile() に渡す（UserのIDは基本的に数値型なので変換が必要）。
+    // サービスから取得した特定ユーザーのプロフィールを返す。
+    return this.usersService.getProfile(+id);
   }
 }
--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,22 +1,39 @@
-import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import {
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
 
+// サービスとしてNestJSのDIコンテナで管理される。
 @Injectable()
 export class UsersService {
-  findAll() {
-    return `This action returns all users`;
+  constructor(
+    //  TypeORM の User エンティティ用リポジトリを注入。これを通じてDB操作を行う。
+    @InjectRepository(User) private readonly userRepo: Repository<User>,
+  ) {}
+
+  // DBから全ユーザーを取得。
+  async findAll(): Promise<User[]> {
+    // .find() はSQLで言えば SELECT * FROM users 相当。
+    return await this.userRepo.find().catch((e) => {
+      // 例外発生時には InternalServerErrorException をスローし、エラー内容も表示。
+      throw new InternalServerErrorException(
+        `[${e.message}]：ユーザーの取得に失敗しました。`,
+      );
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  // id に一致する1件のユーザーを取得。
+  async getProfile(id: number): Promise<User> {
+    // .findOne({ where: { id } }) は SELECT * FROM users WHERE id = ? 相当。
+    const res = await this.userRepo.findOne({ where: { id } });
+    // ユーザーが存在しない場合は NotFoundException をスローして404エラーを返す。
+    if (!res) {
+      throw new NotFoundException(`ユーザーID ${id} が見つかりません`);
+    }
+    return res;
   }
 }
