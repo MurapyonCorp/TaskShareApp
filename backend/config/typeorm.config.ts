bcb9c86487bad601c707158e7d5eb9dab0717f3d
MurapyonCorp <82795887+MurapyonCorp@users.noreply.github.com>
2025-08-25 15:12:20 +0900

    DB configファイルの作成

---
+++ backend/config/typeorm.config.ts
@@ -0,0 +1,15 @@
+import 'dotenv/config';
+import { DataSource } from 'typeorm';
+
+export default new DataSource({
+  type: 'postgres',
+  host: process.env.DATABASE_HOST,
+  port: Number(process.env.DATABASE_PORT),
+  database: process.env.DATABASE_NAME,
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  entities: ['dist/**/entities/**/*.entity.js'],
+  migrations: ['dist/**/migrations/**/*.js'],
+  logging: true,
+  synchronize: false,
+});
