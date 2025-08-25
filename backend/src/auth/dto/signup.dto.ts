7343384b7263f854e9bd1db127a76b5d8d36d192
MurapyonCorp <82795887+MurapyonCorp@users.noreply.github.com>
2025-08-25 15:14:24 +0900

    SignupDto作成

---
+++ backend/src/auth/dto/signup.dto.ts
@@ -0,0 +1,30 @@
+import { IsString, IsNotEmpty, IsEmail, MinLength, IsOptional } from "class-validator";
+import { Match } from "src/common/decorators/match.decorator";
+
+export class SignupDto {
+  @IsString()
+  @IsNotEmpty({ message: '入力必須です' })
+  name: string;
+
+  @IsEmail({}, { message: 'メールアドレスが無効です' })
+  @IsNotEmpty({ message: '入力必須です' })
+  email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  imageId: string;
+
+  @IsString()
+  @IsNotEmpty({ message: '入力必須です' })
+  @MinLength(6)
+  password: string;
+
+  @IsString()
+  @IsNotEmpty({ message: '入力必須です' })
+  @Match('password', { message: 'パスワードが一致していません' })
+  confirmPassword: string;
+
+  @IsOptional()
+  @IsString()
+  introduction?: string;
+}
