25cba38ed20bb70fe7e5aafe278dab32e5e89963
MurapyonCorp <82795887+MurapyonCorp@users.noreply.github.com>
2025-08-25 15:36:30 +0900

    usersのURLを変更

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { UsersService } from './users.service';
 
-@Controller('users')
+@Controller('api/users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
