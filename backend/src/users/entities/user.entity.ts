a262e4cef92d8fa6d9227a843a6b63521f105332
MurapyonCorp <82795887+MurapyonCorp@users.noreply.github.com>
2026-05-06 13:51:23 +0900

    strictPropertyInitializationに対応

--- backend/src/users/entities/user.entity.ts
+++ backend/src/users/entities/user.entity.ts
@@ -12,41 +12,41 @@ export class User extends BaseEntity {
   @PrimaryGeneratedColumn({
     comment: 'ユーザーID',
   })
-  readonly id: number;
+  readonly id!: number;
 
   @Column('varchar', { comment: 'ユーザー名' })
-  name: string;
+  name!: string;
 
   @Column('varchar', { comment: 'メールアドレス' })
-  email: string;
+  email!: string;
 
   @Column('uuid', {
     name: 'image_id',
     nullable: true,
     comment: 'プロフィール画像',
   })
-  imageId: string | null;
+  imageId!: string | null;
 
   @Column('text', { default: '', comment: '自己紹介' })
-  introduction: string;
+  introduction!: string;
 
   @Column('text', {
     name: 'hashed_password',
     nullable: true,
     comment: 'パスワード',
   })
-  hashedPassword: string | null;
+  hashedPassword!: string | null;
 
   @Column('text', {
     name: 'image_url',
     nullable: true,
     comment: '外部プロフィール画像URL',
   })
-  imageUrl: string | null;
+  imageUrl!: string | null;
 
   @CreateDateColumn({ name: 'created_at', comment: '作成日時' })
-  createdAt: Date;
+  createdAt!: Date;
 
   @UpdateDateColumn({ name: 'updated_at', comment: '更新日時' })
-  updatedAt: Date;
+  updatedAt!: Date;
 }
